62aa59985c06decfa3521e1b94639751be5daee3
Boy0211 <s.kuilboer@gmail.com>
2019-01-15 12:38:26 +0100

    2 grafieken in een functie verwerkt

--- artist.js
+++ artist.js
@@ -3,28 +3,36 @@ window.onload = function() {
   d3.json("../Data/artiesten.json").then(function(data) {
 
     console.log(data);
-    drawLineGraphBest(data[332])
-    drawLineGraphWorst(data[332])
+    // drawLineGraphBest(data[12])
+    drawLineGraph("best", data[12])
+    drawLineGraph("worst", data[12])
 
   })
 
 
   function updateLineGraphBest(artist) {
 
-
-
   }
-  function drawLineGraphBest(artist) {
 
-    console.log(artist);
+  function drawLineGraph(graph, artist) {
+
+    if (graph == "best") {
+      var which = "first"
+      var selection = "#bestsong"
+      var selectionclass = "linechartBestSong"
+    } else {
+      var which = "last"
+      var selection = "#worstsong"
+      var selectionclass = "linechartWorstSong"
+    }
 
     var width = 600
     var height = 300
     var margin = {top: 50, bottom: 50, right: 50, left:50}
 
-    var svg = d3.select("#bestsong")
+    var svg = d3.select(selection)
         .append("svg")
-        .attr("class", "linechartBestSong")
+        .attr("class", selectionclass)
         .attr("width", (width + margin.left + margin.right))
         .attr("height", (height + margin.top + margin.bottom))
       .append("g")
@@ -42,11 +50,9 @@ window.onload = function() {
 
     var line = d3.line()
         .x(function (d) {
-          console.log(d.Lijst);
           return xScale(d.Lijst); })
         .y(function (d) {
-          console.log(d.Data[0].Notering);
-          return yScale(d.Data[0].Notering); })
+          return yScale(d.Data[getFirstOrLast(which, d.Data)].Notering) })
 
     svg.append("g")
         .attr("class", "x-axis")
@@ -62,53 +68,13 @@ window.onload = function() {
         .attr("class", "line")
         .attr("d", line(artist["TOP2000JAAR"]))
   }
+}
 
-  function drawLineGraphWorst(artist) {
+function getFirstOrLast(which, data) {
 
-    console.log(artist);
-
-    var width = 600
-    var height = 300
-    var margin = {top: 50, bottom: 50, right: 50, left:50}
-
-    var svg = d3.select("#worstsong")
-        .append("svg")
-        .attr("class", "linechartWorstSong")
-        .attr("width", (width + margin.left + margin.right))
-        .attr("height", (height + margin.top + margin.bottom))
-      .append("g")
-        .attr("transform", "translate(" + margin.top + "," + margin.left + ")");
-
-    var parseTime = d3.timeParse("%Y")
-    var bisectDate = d3.bisector(function(d) {return d.Lijst})
-
-    var xScale = d3.scaleTime()
-        .range([0, width])
-        .domain(d3.extent(artist["TOP2000JAAR"], function(d) {return d.Lijst}))
-    var yScale = d3.scaleLinear()
-        .range([height, 0])
-        .domain([0, 2000])
-
-    var line = d3.line()
-        .x(function (d) {
-          console.log(d.Lijst);
-          return xScale(d.Lijst); })
-        .y(function (d) {
-          console.log(d.Data[d.Data.length - 1].Notering);
-          return yScale(d.Data[d.Data.length - 1].Notering); })
-
-    svg.append("g")
-        .attr("class", "x-axis")
-        .call(d3.axisBottom(xScale))
-        .attr("transform", "translate(0," + (height) + ")")
-
-    svg.append("g")
-        .attr("class", "y-axis")
-        .call(d3.axisLeft(yScale))
-
-    svg.append("g").append("path")
-        .datum(artist["TOP2000JAAR"])
-        .attr("class", "line")
-        .attr("d", line(artist["TOP2000JAAR"]))
+  if (which == "first") {
+    return 0
+  } else {
+    return (data.length - 1)
   }
 }
